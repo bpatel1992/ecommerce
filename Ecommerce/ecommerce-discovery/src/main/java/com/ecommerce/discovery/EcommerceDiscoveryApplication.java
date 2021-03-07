a0c1463f920c9dc4bdc05d66f1e66f89b337f9d9
Bhupendra <bhupendrapatel1992@gmail.com>
2021-03-07 10:55:38 +0530

    new file added

---
+++ Ecommerce/ecommerce-discovery/src/main/java/com/ecommerce/discovery/EcommerceDiscoveryApplication.java
@@ -0,0 +1,17 @@
+package com.ecommerce.discovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EcommerceDiscoveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceDiscoveryApplication.class, args);
+
+	}
+
+}
